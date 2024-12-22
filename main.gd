886b5dff2808234ddd6c3488e5c56725204a30fe
Rajesh J Advani <rajeshjadvani@yahoo.com>
2024-12-22 19:13:07 +0530

    Initial Screen scrolling.

---
+++ main.gd
@@ -0,0 +1,9 @@
+# Scene structure (save as main.tscn):
+# Main (Node2D)
+#  ├── Background (Node2D)
+#  │    ├── BGSprite1 (Sprite2D)
+#  │    └── BGSprite2 (Sprite2D)
+#  └── Player (Sprite2D)
+
+# main.gd
+extends Node2D
